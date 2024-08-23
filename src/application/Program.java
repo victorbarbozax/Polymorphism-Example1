66b523abf6b5d42daa835c38573bab50a64f0e9f
Victor Barboza <victor.barboza052008@gmail.com>
2024-08-23 15:23:58 -0300

    Updated classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,39 +21,43 @@ public class Program {
 		
 		List<Product> list = new ArrayList<>();
 		
-		System.out.print("Entre com o numero de produtos: ");
+		System.out.println("Entre com o numero de produtos: ");
 		int n = sc.nextInt();
 		
-		for (int i=1; i<=n; i++) {
-			System.out.println("Datos do produto #" + i + ":");
-			System.out.print("Comum, usado, importado (c/u/i)? ");
-			char type = sc.next().charAt(0);
-			System.out.print("Nome: ");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Preco: ");
-			double price = sc.nextDouble();
-			if (type == 'c') {
-				list.add(new Product(name, price));
-			}
-			else if (type == 'u') {
-				System.out.print("Data de fabricacao (DD/MM/YYYY): ");
-				LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-				list.add(new UsedProduct(name, price, date));
-			}
-			else {
-				System.out.print("Taxa de importacao: ");
-				double customsFee = sc.nextDouble();
-				list.add(new ImportedProduct(name, price, customsFee));
-			}
+		for (int i = 1; i<=n; i++) {
+			System.out.println("Dados do produto #" + i + ":");
+	        System.out.print("Comum, usado, importado (c/u/i)? ");
+	        char type = sc.next().charAt(0);
+	        System.out.print("Nome: ");
+	        sc.nextLine();
+	        String name = sc.nextLine();
+	        System.out.print("Preco: ");
+	        double price = sc.nextDouble();
+	        if (type =='c') {
+	        	list.add(new Product(name, price));
+	        }
+	        else if(type == 'u') {
+	        	System.out.print("Data de fabricacao (DD/MM/YYYY: ");
+	        	LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	        	list.add(new UsedProduct(name, price, date));
+	        }
+		
+	        else {
+	        	System.out.print("Taxa de importacao: ");
+	        	double customsFee = sc.nextDouble();
+	        	list.add(new ImportedProduct(name, price, customsFee));
 		}
-
-		System.out.println();
-		System.out.println("ETIQUETAS DE PRECO:");
-		for (Product prod : list) {
-			System.out.println(prod.priceTag());
-		}
+		
+	        
+	        System.out.println();
+	        System.out.println("ENTIQUETAS DE PRECO:");
+		    for(Product prod : list) {
+		    	System.out.println(prod.priceTag());
+		    }
+		
+		    System.out.println();
 		
 		sc.close();
 	}
+}
 }
